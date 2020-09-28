cd0b1d03b93239c5055a3162576e5418d9d0263c
fervilla <jorge.villanueva@wizeline.com>
2020-09-28 11:43:24 -0500

    fix: dependency added to useeffect hook

--- src/providers/Search/Search.provider.jsx
+++ src/providers/Search/Search.provider.jsx
@@ -29,33 +29,39 @@ function SearchBarProvider({ children }) {
     axiosCall.get('/search', { params }).then((apiResult) => {
       setVideos(apiResult.data);
     });
-  }, []);
+  }, [REACT_APP_YOUTUBE_API_KEY]);
 
-  const searchVideo = useCallback(async (search) => {
-    const axiosCall = axiosClient();
-    const params = {
-      part: 'snippet',
-      key: `${REACT_APP_YOUTUBE_API_KEY}`,
-      type: ['video'],
-      maxResults: 12,
-      q: search,
-    };
-    const searchedVideos = await axiosCall.get('/search', { params });
-    setVideos(searchedVideos.data);
-  }, []);
+  const searchVideo = useCallback(
+    async (search) => {
+      const axiosCall = axiosClient();
+      const params = {
+        part: 'snippet',
+        key: `${REACT_APP_YOUTUBE_API_KEY}`,
+        type: ['video'],
+        maxResults: 12,
+        q: search,
+      };
+      const searchedVideos = await axiosCall.get('/search', { params });
+      setVideos(searchedVideos.data);
+    },
+    [REACT_APP_YOUTUBE_API_KEY]
+  );
 
-  const searchRecomendations = useCallback(async (videoId) => {
-    const axiosCall = axiosClient();
-    const params = {
-      part: 'snippet',
-      key: `${REACT_APP_YOUTUBE_API_KEY}`,
-      type: 'video',
-      maxResults: 3,
-      relatedToVideoId: videoId,
-    };
-    const relatedVideos = await axiosCall.get('/search', { params });
-    setRecomendations(relatedVideos.data);
-  }, []);
+  const searchRecomendations = useCallback(
+    async (videoId) => {
+      const axiosCall = axiosClient();
+      const params = {
+        part: 'snippet',
+        key: `${REACT_APP_YOUTUBE_API_KEY}`,
+        type: 'video',
+        maxResults: 3,
+        relatedToVideoId: videoId,
+      };
+      const relatedVideos = await axiosCall.get('/search', { params });
+      setRecomendations(relatedVideos.data);
+    },
+    [REACT_APP_YOUTUBE_API_KEY]
+  );
 
   return (
     <SearchBarContext.Provider
